a6fe770b23b100ca1150eba7346f295b2494705a
Steven Rico <steven@intuitiveprogrammer.com>
2019-02-04 22:00:55 +0000

    Created Tab component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import TabLayout from './layouts/TabLayout'
+import Tab from './components/TabLayout/Tab'
 
 const App = () => (
   <TabLayout>
     <TabLayout.Nav>
-
+      <Tab label="one" />
+      <Tab label="two" />
     </TabLayout.Nav>
   </TabLayout>
 )
---
+++ src/components/TabLayout/Tab.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tab = ({label}) => (
+  <div className="tab-layout__nav__tab">
+    {label.toUpperCase()}
+  </div>
+)
+
+export default Tab
