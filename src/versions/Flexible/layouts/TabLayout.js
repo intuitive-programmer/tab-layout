d3699742a4a4407ea24e44a090cec368a4fe4f0a
Steven Rico <steven@intuitiveprogrammer.com>
2019-02-05 16:32:49 +0000

    Created Flexible compound component using context.

--- src/versions/Flexible/layouts/TabLayout.js
+++ src/versions/Flexible/layouts/TabLayout.js
@@ -1,37 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component, createContext } from 'react'
 
 import Nav from '../components/TabLayout/Nav'
 import Content from '../components/TabLayout/Content'
 
+const TabLayoutContext = createContext({})
+
+const TabLayoutConsumer = props => {
+  return(
+    <TabLayoutContext.Consumer {...props}>
+      {context => {
+        if (!context) {
+          throw new Error(
+            'TabLayout compound components cannot be rendered outside the TabLayout Component.'
+          )
+        }
+        return props.children(context)
+      }}
+    </TabLayoutContext.Consumer>
+  )
+}
+
 class TabLayout extends Component {
-  static Nav = ({children, activeIndex, activateTab}) => (
-    <Nav
-      activeIndex={activeIndex}
-      activateTab={activateTab}
-    >{children}</Nav>
+  static Nav = ({children}) => (
+    <TabLayoutConsumer>
+      {context => {
+        const { activeIndex, activateTab } = context
+        return(
+          <Nav
+            activeIndex={activeIndex}
+            activateTab={activateTab}
+          >{children}</Nav>
+        )
+      }}
+    </TabLayoutConsumer>
   )
 
-  static Content =({children, activeIndex}) => (
-    <Content
-      activeIndex={activeIndex}
-    >{children}</Content>
+  static Content =({children}) => (
+    <TabLayoutConsumer>
+      {context => {
+        const { activeIndex } = context
+        return(
+          <Content
+            activeIndex={activeIndex}
+          >{children}</Content>
+        )
+      }}
+    </TabLayoutConsumer>
   )
+
+  activateTab = tabIndex => this.setState({ activeIndex: tabIndex })
 
   state = {
     activeIndex: 0,
+    activateTab: this.activateTab,
   }
-
-  activateTab = tabIndex => this.setState({ activeIndex: tabIndex })
 
   render() {
     const { state, props } = this
     return(
-      React.Children.map(props.children, child =>
-        React.cloneElement(child, {
-          activeIndex: state.activeIndex,
-          activateTab: this.activateTab,
-        })  
-      )
+      <TabLayoutContext.Provider value={state}>
+        {props.children}
+      </TabLayoutContext.Provider>  
     )
   }
 }
